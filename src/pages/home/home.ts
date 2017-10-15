e226829bfb64b911d2a6609ef6a539141a0cbe62
mohamed essam <mohamed.essam9393@gmail.com>
2017-10-15 16:22:55 +0200

    second commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,57 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewChild, ElementRef } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation';
 
+declare var  google;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public lat : number;
+  public map: any;
+  
+  public lng : number;
+  @ViewChild('map') mapElement: ElementRef;
+  constructor(public navCtrl: NavController,public geolocation: Geolocation,) {
+this.loadMap();
+  }
+  
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      this.lat = resp.coords.latitude;
+      this.lng = resp.coords.longitude;
+        let mapOptions = {
+          center: latLng,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-  constructor(public navCtrl: NavController) {
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      google.maps.event.addListener(this.map, 'click', (event) => {
+        var location  = event.latLng;
+        this.lat = location.lat();
+        this.lng = location.lng();
+        console.log(this.lat);
+        console.log(this.lng);
+        // this.addMarker(location);
+      });
+      this.addMarker(this.map.getCenter());
+    
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
 
   }
 
+  addMarker(LatLng){
+    let marker  = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: LatLng
+    });
+  
+  }
 }
